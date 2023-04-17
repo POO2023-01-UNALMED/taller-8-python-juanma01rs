622a7be877050ddd182de7419288c8e99155bff9
juanma01rs <juanma10rs@gmail.com>
2023-04-17 17:51:24 -0500

    13

--- futbolista.py
+++ futbolista.py
@@ -1,58 +1,11 @@
-class Persona: 
-    
-    def __init__(self, nombre, edad, altura, sexo):
-        self._nombre = nombre
-        self._edad = edad
-        self._altura = altura
-        self._sexo = sexo
-    
-    def setNombre(self, nombre):    
-        self._nombre = nombre
-        
-    def getNombre(self):
-        return self._nombre
-          
-    def setEdad(self, edad):    
-        self._edad = edad
-        
-    def getEdad(self):
-        return self._edad
-    
-    def setAltura(self, altura):    
-        self._altura = altura
-        
-    def getAltura(self):
-        return self._altura    
-    
-    def setSexo(self, sexo):    
-        self._sexo = sexo
-        
-    def getSexo(self):
-        return self._sexo  
-    
-class Deportista:
-     
-    def __init__ (self, deporte = "Futbol", añosPracticando = 0):
-        self._deporte = deporte
-        self._añosPracticando = añosPracticando
-    
-    def setDeporte(self, deporte):    
-        self._deporte = deporte
-        
-    def getDeporte(self):
-        return self._deporte  
-    
-    def setAñosPracticando(self, añosPracticando):    
-        self._añosPracticando = añosPracticando
-        
-    def getAñosPracticando(self):
-        return self._añosPracticando
+from persona import Persona
+from deportista import Deportista
 
 class Futbolista(Persona, Deportista):
     
     _listaFutbolistas = []
     
-    def __init__(self, nombre, edad, altura, sexo, deporte, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+    def __init__(self, nombre, edad, altura, sexo, deporte = 'Futbol', añosPracticando = 0, golesMarcados = 0, tarjetasRojas = 0, piernaHabil = 0):
         Persona.__init__(self, nombre, edad, altura, sexo)
         Deportista.__init__(self, deporte, añosPracticando)
         self._golesMarcados = golesMarcados
